3ed7ff93c00c0244b6b003ba693e4f5492bd5878
Dunkel <89481059+Dunkel404@users.noreply.github.com>
2025-08-01 18:41:53 +0000

    layout de item do cardapio atualizado

--- src/app/tests/page.jsx
+++ src/app/tests/page.jsx
@@ -8,17 +8,15 @@ export default function page() {
                     <div id="item-image">
                         <Image src="/img/coca.webp" fill={true} quality={50} alt="coca" />
                     </div>
-                    <div id="item-buy">
-                    <button onclick="addToCart('Coca Cola lata 250ml')"/>
-                    </div>
+                    <div id="buy-button" onclick="addToCart('Coca Cola lata 250ml')"> <h1>+</h1></div>
                     <div id="item-price">
-                        <Image src="/img/coca.webp" fill={true} quality={50} alt="coca" />
+                        <span> R$20,40</span>
                     </div>
                     <div id="item-name">
                     <h2> Coca Cola </h2>
                     </div>
                     <div id="item-info">
-                        <Image src="/img/coca.webp" fill={true} quality={50} alt="coca" />
+                        <p> coca cola legal saborosa </p>
                     </div>
                                         <div id="item-status">
                     <p id="status">Item não disponivel</p>
