a29c8abd8ea5d45847798e225e8a8f9de7846328
Dunkel404 <matheusemartins694@proton.me>
2025-08-01 23:32:13 -0300

    testes e layouts de itens

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,21 +1,21 @@
 import Link from 'next/link';
 
-export default function HomePage(){
+export default function HomePage() {
     return (
-            <section id="home">
-        <div id="texts">
-            <h1>As Melhores Pizzas da Cidade</h1>
-            <h3>Feitas com ingredientes frescos e amor</h3>
-        </div>
-        <div id="buttons">
-            <Link href={'Cardapio.html'}>
-            {/* onclick="window.location.href='Cardapio.html'" */}
-                <button id="menu-btn" >Ver Cardápio</button>
-            </Link>
-            <Link href={'Cardapio.html'}>
-              <button id="table-btn">Reservar Mesa</button>
-            </Link>
-        </div>
-    </section>
+        <section id="home">
+            <div id="texts">
+                <h1>As Melhores Pizzas da Cidade</h1>
+                <h3>Feitas com ingredientes frescos e amor</h3>
+            </div>
+            <div id="buttons">
+                <Link href={'Cardapio.html'}>
+                    {/* onclick="window.location.href='Cardapio.html'" */}
+                    <button id="menu-btn" >Ver Cardápio</button>
+                </Link>
+                <Link href={'Cardapio.html'}>
+                    <button id="table-btn">Reservar Mesa</button>
+                </Link>
+            </div>
+        </section>
     )
 }
