885525f5d0806e8ff0c1ab0bb65c0d7040634a8d
Dunkel404 <matheusemartins694@proton.me>
2025-08-05 23:08:15 -0300

    menu atualizado

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,25 +1,37 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function HomePage() {
 
     return (
         <section id="home">
-            <div id="texts">
-                <h1>As Melhores Pizzas da Cidade</h1>
-                <h3>Feitas com ingredientes frescos e amor</h3>
+            <div id="info">
+                <div id="texts">
+                    <h1>As Melhores Pizzas, bem perto de você</h1>
+                    <h3>Ingredientes de primeira e massa fresca, com o cuidado que você sente em cada fatia.</h3>
+                </div>
+                <div id="buttons">
+                    <Link href={'Cardapio.html'}>
+                        {/* onclick="window.location.href='Cardapio.html'" */}
+                        <button id="menu-btn" >Ver Cardápio</button>
+                    </Link>
+                    <Link href={'Cardapio.html'}>
+                        <button id="table-btn">Reservar Mesa</button>
+                    </Link>
+
+                </div>
             </div>
-            <div id="buttons">
-                <Link href={'Cardapio.html'}>
-                    {/* onclick="window.location.href='Cardapio.html'" */}
-                    <button id="menu-btn" >Ver Cardápio</button>
-                </Link>
-                <Link href={'Cardapio.html'}>
-                    <button id="table-btn">Reservar Mesa</button>
-                </Link>
+            <div id="promotional">
+                <div id="pizzaPhoto">
+                    <Image
+                        src={"/img/home-background.jpg"}
+                        width={0}
+                        height={0}
+                        sizes='100vw'
+                        style={{ width: 'auto', height: '100%'}}
+                    />
+                </div>
             </div>
         </section>
-
-
-
     )
 }
